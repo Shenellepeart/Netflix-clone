c3460ea6bbc270c37bbf0083cd24f8767a5dd0c8
Shenellepeart <shenelledpeart@gmail.com>
2023-06-16 16:54:47 -0500

    Added API to test api call

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const axios = require("axios");
 const app = express();
 const port = 4000;
 const baseURL = "https://api.themoviedb.org/3";
+const API_KEY = "2b51dd28752f54b88383f986dd74797a";
+const fetchTrending = `/trending/all/week?api_key=${API_KEY}&language=en-US`
 
 app.use(cors());
 
 app.get("/data", async (req, res) => {
   try {
-    const response = await axios.get(baseURL);
+    const response = await axios.get(`${baseURL}${fetchTrending}`);
     res.json(response.data);
   } catch (error) {
     console.log(error);
